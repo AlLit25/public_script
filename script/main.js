bb78d8b4e79e0397a93d6d5f793b2f8a6da66bbe
Oleksandr Lytvin <allitwin25@gmail.com>
2025-06-29 13:44:16 +0300

    connect template

--- script/main.js
+++ script/main.js
@@ -4,6 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 class Main {
     constructor() {
-        console.log('Welcome');
+        this.main = document.querySelector('main');
+
+        if (Main.isset(this.main)) {
+            Main.connectTemplate(this.main, 'home');
+        }
+
+    }
+
+    static isset(elem) {
+        return elem !== null && elem !== undefined;
+    }
+
+    static connectTemplate(parent, nameTemplate) {
+        fetch(`template/${nameTemplate}.html`)
+            .then(response => response.text())
+            .then(data => {
+                parent.insertAdjacentHTML('afterbegin', data);
+            })
+            .catch(error => console.error('Error loading header:', error));
     }
 }
